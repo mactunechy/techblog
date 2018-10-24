68faca4161556c8609d5441cb29154055db87c00
mactunechy <mactunechy@gmail.com>
2018-10-25 01:16:38 +0200

    fixing loging in

--- techsite/techsite/urls.py
+++ techsite/techsite/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views
+from django.shortcuts import redirect
+
+def next_home(request):
+    return redirect('post_list')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('',include('blog.urls')),
-    path('accounts/login',views.auth_login,name = 'login'),
-    path('accounts/logout', views.auth_logout,name='logout',kwargs={'next_page':'/'}),
+    path('accounts/login',views.LoginView.as_view(),name = 'login',kwargs={'next':'/'}),
+    path('accounts/logout', views.LogoutView.as_view(),name='logout',kwargs={'next_page':'/'}),
+    path('accounts/next',next_home)
 ]
